33e57efeb65434d2b5a85203614aa2da2ac47d98
Jay Deleskie <jaydeleskie@gmail.com>
2025-10-26 14:35:11 -0700

    Final: vercel.json + clean server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('Property Portfolio API is running!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
